d9aa7295c7e116597b64555419f460e1385fe127
Morten Holt <thawk@t-hawk.com>
2014-07-02 21:39:39 +0200

    Added initial placeholder bundle

---
+++ src/mholt/txtAHolic/ServerBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mholt\txtAHolic\ServerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ServerBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
